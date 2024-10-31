5e0084e6c2f30d3a96cde415c3df5fd37670e817
tsabit ghazwan <tsabit.ghazwan@thetreedots.com>
2024-10-31 14:14:01 +0700

    fix: comment temporary

--- api/src/services/email.services.ts
+++ api/src/services/email.services.ts
@@ -9,29 +9,29 @@ export class EmailService {
   private transporter: nodemailer.Transporter;
 
   constructor(private configService: ConfigService) {
-    const clientId = this.configService.get('EMAIL_CLIENT_ID');
-    const clientSecret = this.configService.get('EMAIL_CLIENT_SECRET');
-    const redirectUri = this.configService.get('EMAIL_REDIRECT_URI');
-    const refreshToken = this.configService.get('EMAIL_REFRESH_TOKEN');
+    // const clientId = this.configService.get('EMAIL_CLIENT_ID');
+    // const clientSecret = this.configService.get('EMAIL_CLIENT_SECRET');
+    // const redirectUri = this.configService.get('EMAIL_REDIRECT_URI');
+    // const refreshToken = this.configService.get('EMAIL_REFRESH_TOKEN');
 
-    this.oauth2Client = new google.auth.OAuth2(
-      clientId,
-      clientSecret,
-      redirectUri,
-    );
-    this.oauth2Client.setCredentials({ refresh_token: refreshToken });
+    // this.oauth2Client = new google.auth.OAuth2(
+    //   clientId,
+    //   clientSecret,
+    //   redirectUri,
+    // );
+    // this.oauth2Client.setCredentials({ refresh_token: refreshToken });
 
-    this.transporter = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: 'sib3.klhk@gmail.com',
-        clientId: clientId,
-        clientSecret: clientSecret,
-        refreshToken: refreshToken,
-        accessToken: this.getAccessToken(), // Automatically refreshes the token
-      },
-    });
+    // this.transporter = nodemailer.createTransport({
+    //   service: 'gmail',
+    //   auth: {
+    //     type: 'OAuth2',
+    //     user: 'sib3.klhk@gmail.com',
+    //     clientId: clientId,
+    //     clientSecret: clientSecret,
+    //     refreshToken: refreshToken,
+    //     accessToken: this.getAccessToken(), // Automatically refreshes the token
+    //   },
+    // });
   }
 
   private async getAccessToken(): Promise<string> {
@@ -45,22 +45,22 @@ export class EmailService {
     text: string,
     html?: string,
   ): Promise<void> {
-    console.log(this.configService.get('EMAIL_USER'));
-    console.log(this.configService.get('EMAIL_PASSWORD'));
-    const mailOptions = {
-      from: `"SIB3 KLHK" <${this.configService.get('EMAIL_USER')}>`,
-      to,
-      subject,
-      text,
-      html,
-    };
+    // console.log(this.configService.get('EMAIL_USER'));
+    // console.log(this.configService.get('EMAIL_PASSWORD'));
+    // const mailOptions = {
+    //   from: `"SIB3 KLHK" <${this.configService.get('EMAIL_USER')}>`,
+    //   to,
+    //   subject,
+    //   text,
+    //   html,
+    // };
 
-    try {
-      await this.transporter.sendMail(mailOptions);
-      console.log(`Email sent to ${to}`);
-    } catch (error) {
-      console.error(`Error sending email: ${error.message}`);
-      throw new Error('Email failed to send');
-    }
+    // try {
+    //   await this.transporter.sendMail(mailOptions);
+    //   console.log(`Email sent to ${to}`);
+    // } catch (error) {
+    //   console.error(`Error sending email: ${error.message}`);
+    //   throw new Error('Email failed to send');
+    // }
   }
 }
